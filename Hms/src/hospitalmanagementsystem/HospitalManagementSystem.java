e1433fb628ca69a110faf76d0f668c3965eb68f5
Muhammad Tayyab <156538622+TayyabXtreme@users.noreply.github.com>
2025-01-12 19:42:45 +0500

    hms in java

---
+++ Hms/src/hospitalmanagementsystem/HospitalManagementSystem.java
@@ -0,0 +1,35 @@
+
+package hospitalmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class HospitalManagementSystem extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setMinWidth(340);
+        stage.setMinHeight(580);
+        
+        stage.setTitle("Hospital Management System");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
