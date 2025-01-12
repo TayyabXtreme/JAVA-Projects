efbd08e0498dc8240ba045a8a2be28a8be7db047
Muhammad Tayyab <156538622+TayyabXtreme@users.noreply.github.com>
2025-01-12 19:44:43 +0500

    jfx first

---
+++ jfxfirst/src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setTitle("FirstHello");
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		try {
+			Parent root =FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+			Stage stage=new Stage();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
